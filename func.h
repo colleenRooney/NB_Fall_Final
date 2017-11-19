fb1ed63e8e8f4398802917419f476d02415b7ae7
Colleen Rooney <crooney@ada.cs.pdx.edu>
2017-11-19 14:00:39 -0800

    made sanatizeInput more readable

--- func.h
+++ func.h
@@ -2,34 +2,86 @@
 #define MAX_NAME 20
 
 /*********************************************\
-* sanitizeInput
+*isaLetter
+* returns true if character is a letter
+\**********************************************/
+int isaLetter(char letter) //if character is a letter return 1, else return 0
+
+{
+	if(((letter <= 122) && (letter >= 97)) || ((letter <= 90) && (letter >= 65)))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/*********************************************\
+*isaSpace
+* returns true if character is a space
+\**********************************************/
+int isaSpace(char letter)
+{
+	if(letter == 32) return 1;
+	else return 0;
+}
+
+/*********************************************\
+*decap
+* decaps input if input is uppercase
+\**********************************************/
+void decap(char *letter) //function returns lowercase equivalent of uppercase letter
+{
+	if((*letter <= 90) && (*letter >= 65)) 
+	{
+		*letter = *letter + 32;
+	}
+	return;
+}
+ 
+/*********************************************\
+*capitalize
+* capitalizes input if input is lowercase
+\**********************************************/
+void capitalize(char *letter) //function returns uppercase equivalent of lowercase letter
+{
+	if((*letter <= 122) && (*letter >= 97))
+		{
+			*letter = *letter - 32;
+		}
+	return;
+}
+
+/*********************************************\
+*sanitizeInput
 * modifies a string into a standard format
 \**********************************************/
 void sanitizeInput(char input[])
 {
-	int length=strlen(input);
-	for(int i=0;i<length;i++)
+	int length = strlen(input);
+
+	for(int i = 0; i < length; i++)
 	{
-		if(input[i]=='\n')
+		if(input[i] == '\n')
 		{
-			input[i]='\0';
+			input[i] = '\0'; //terminate string
 		}
-		else if(i==0 || input[i-1]==32)
+		else if(i == 0 || isaSpace(input[i-1])) //if first letter or new word
 		{
-			if(input[i]>=97 && input[i]<=122) input[i]=input[i]-32; //Capitalize lowercase letter at start or if a new word
+			capitalize(&input[i]);
 		}
-		else if(input[i]==32 && input[i+1]<65 || input[i]>122 || (input[i]>90 && input[i]<97))
+		else if(isaSpace(input[i]) && !isaLetter(input[i+1])) //if letter is a space without a following letter
 		{
-			input[i]='\0'; //letter is a space and another letter does not follow, change to string terminator
+			input[i]='\0'; //terminate string
 		}
 		else
 		{
-			if(input[i]>=65 && input[i]<=90) input[i]=input[i]+32; //decapatilzes uppercase letters that shoudld be lower case
+			decap(&input[i]);
 		}
 	}
 }
-
-
 
 /*************************************************************
 *createMap
